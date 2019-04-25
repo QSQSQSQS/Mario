852b4b294e3d81057c8e52b1f89855d459d3f0c9
shixiaohanforgithub <48573717+shixiaohanforgithub@users.noreply.github.com>
2019-04-25 11:26:36 +0800

    Update KeyInput.java

--- src/Input/KeyInput.java
+++ src/Input/KeyInput.java
@@ -15,11 +15,11 @@ public class KeyInput implements KeyListener {
 		for(Entity en: Game.handler.entity) {
 			switch(key) {
 			case KeyEvent.VK_W:
-				en.setVelY(-5);
-				break;
-			case KeyEvent.VK_S:
-				en.setVelY(-5);
-				break;
+				if(!en.jumping) {
+					en.jumping = true;
+					en.gravity = 10.0;
+				}
+				break;			
 			case KeyEvent.VK_A:
 				en.setVelX(-5);
 				break;
